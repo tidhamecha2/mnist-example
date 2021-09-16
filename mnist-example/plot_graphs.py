6971bd2ca740ab24db28245ee73fd38cadbde6d9
Tejas <tidhamecha@in.ibm.com>
2021-09-17 01:07:13 +0530

    model selection based on f1 on validation set

--- mnist-example/plot_graphs.py
+++ mnist-example/plot_graphs.py
@@ -42,8 +42,8 @@ digits = datasets.load_digits()
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, label in zip(axes, digits.images, digits.target):
     ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    ax.set_title('Training: %i' % label)
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+    ax.set_title("Training: %i" % label)
 
 ###############################################################################
 # Classification
@@ -62,62 +62,68 @@ for ax, image, label in zip(axes, digits.images, digits.target):
 
 # flatten the images
 n_samples = len(digits.images)
-print("Image size is:")
-print(digits.images[0].shape)
 
-rescale_factors = [0.5, 1, 2]
-test_sizes = [0.1, 0.2, 0.3, 0.4]
-for rescale_factor in rescale_factors:
-    resized_images=[]
-    for d  in digits.images:
-        resized_images.append(rescale(d, rescale_factor, anti_aliasing=False))
-    for test_size in test_sizes:
-        
+rescale_factors = [0.25, 0.5, 1, 2, 3]
 
-        resized_images = np.array(resized_images)
-        data = resized_images.reshape((n_samples, -1))
+for test_size, valid_size in [(0.15, 0.15), (0.20, 0.10)]:
+    for rescale_factor in rescale_factors:
+        model_candidates = []
+        for gamma in [10 ** exponent for exponent in range(-7, 0)]:
+            resized_images = []
+            for d in digits.images:
+                resized_images.append(rescale(d, rescale_factor, anti_aliasing=False))
 
-        # Create a classifier: a support vector classifier
-        clf = svm.SVC(gamma=0.001)
+            resized_images = np.array(resized_images)
+            data = resized_images.reshape((n_samples, -1))
 
-        # Split data into 50% train and 50% test subsets
-        X_train, X_test, y_train, y_test = train_test_split(
-            data, digits.target, test_size=test_size, shuffle=False)
+            # Create a classifier: a support vector classifier
+            clf = svm.SVC(gamma=gamma)
 
-        # Learn the digits on the train subset
-        clf.fit(X_train, y_train)
+            X_train, X_test_valid, y_train, y_test_valid = train_test_split(
+                data, digits.target, test_size=test_size + valid_size, shuffle=False
+            )
 
+            X_test, X_valid, y_test, y_valid = train_test_split(
+                X_test_valid,
+                y_test_valid,
+                test_size=valid_size / (test_size + valid_size),
+                shuffle=False,
+            )
+
+            # print("Number of samples: Train:Valid:Test = {}:{}:{}".format(len(y_train),len(y_valid),len(y_test)))
+
+            # Learn the digits on the train subset
+            clf.fit(X_train, y_train)
+            predicted_valid = clf.predict(X_valid)
+            acc_valid = metrics.accuracy_score(y_pred=predicted_valid, y_true=y_valid)
+            f1_valid = metrics.f1_score(
+                y_pred=predicted_valid, y_true=y_valid, average="macro"
+            )
+            candidate = {
+                "model": clf,
+                "acc_valid": acc_valid,
+                "f1_valid": f1_valid,
+                "gamma": gamma,
+            }
+            model_candidates.append(candidate)
         # Predict the value of the digit on the test subset
-        predicted = clf.predict(X_test)
 
-        ###############################################################################
-        # Below we visualize the first 4 test samples and show their predicted
-        # digit value in the title.
-
-        _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-        for ax, image, prediction in zip(axes, X_test, predicted):
-            ax.set_axis_off()
-            image = image.reshape(resized_images[0].shape)
-            ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-            ax.set_title(f'Prediction: {prediction}')
-
-        ###############################################################################
-        # :func:`~sklearn.metrics.classification_report` builds a text report showing
-        # the main classification metrics.
-
-        # print(f"Classification report for classifier {clf}:\n"
-        #     f"{metrics.classification_report(y_test, predicted)}\n")
-
-        ###############################################################################
-        # We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-        # true digit values and the predicted digit values.
-
-        disp = metrics.plot_confusion_matrix(clf, X_test, y_test)
-        disp.figure_.suptitle("Confusion Matrix")
-        # print(f"Confusion matrix:\n{disp.confusion_matrix}")
+        max_valid_f1_model_candidate = max(
+            model_candidates, key=lambda x: x["f1_valid"]
+        )
+        predicted = max_valid_f1_model_candidate["model"].predict(X_test)
 
         acc = metrics.accuracy_score(y_pred=predicted, y_true=y_test)
-        f1 = metrics.f1_score(y_pred=predicted, y_true=y_test, average='macro')
-        print("{}x{}\t{}:{}\t{}\t{}".format(resized_images[0].shape[0], resized_images[0].shape[1], (1-test_size)*100,test_size*100, acc,f1))
+        f1 = metrics.f1_score(y_pred=predicted, y_true=y_test, average="macro")
+        print(
+            "{}x{}\t{}\t{}:{}\t{:.3f}\t{:.3f}".format(
+                resized_images[0].shape[0],
+                resized_images[0].shape[1],
+                max_valid_f1_model_candidate["gamma"],
+                (1 - test_size) * 100,
+                test_size * 100,
+                acc,
+                f1,
+            )
+        )
 
-        plt.show()
