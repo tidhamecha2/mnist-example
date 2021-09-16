446edfa6f6ac9f276ac0c9628fb97f100a92a656
Tejas <tidhamecha@in.ibm.com>
2021-09-17 01:09:12 +0530

    removed  image demo code snippet

--- mnist-example/plot_graphs.py
+++ mnist-example/plot_graphs.py
@@ -39,12 +39,6 @@ import numpy as np
 
 digits = datasets.load_digits()
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Training: %i" % label)
-
 ###############################################################################
 # Classification
 # --------------
